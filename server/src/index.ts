c9be6c93934985e869eded7c0f0cccbcc692b9f9
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-04-30 11:52:53 +0200

    Set up express server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+import http from "http";
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(compression());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+const PORT = 8080;
+
+server.listen(8080, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});

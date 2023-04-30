5df35608e247a0e99be8664db8cadeeec816cfdf
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-04-30 12:29:47 +0200

    Fix the typo

--- server/src/helpers/index.ts
+++ server/src/helpers/index.ts
@@ -2,7 +2,7 @@ import crypto from "crypto"
 
 const SECRET = "E-COMMERCE-WEBSITE-REST-API"
 
-export const rando = () => crypto.randomBytes(128).toString("base64")
+export const random = () => crypto.randomBytes(128).toString("base64")
 export const authentication = (salt: string, password: string) => {
   return crypto
     .createHmac("sha256", [salt, password].join("/"))

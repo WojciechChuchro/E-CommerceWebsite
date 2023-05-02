3425739e2b256510dcf711869a923b7a4006d489
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-05-02 19:31:47 +0200

    Create register component.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,15 +2,12 @@ import { useNavigate } from "react-router-dom"
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
-const Header = () => {
-  const navigate = useNavigate()
-  const handleLogin = () => navigate("/login")
-  const handleRegister = () => navigate("/register")
 
+const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="">Navbar</Navbar.Brand>
+        <Navbar.Brand href="">E-Commerce Website!</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/login">Login</Nav.Link>
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,8 +1,74 @@
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 const Register = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState<String>()
+  const [username, setUsername] = useState<String>()
+  const navigate = useNavigate()
+
+  const onEmail = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(event.target.value)
+
+  const onPassword = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setPassword(event.target.value)
+  const onUsername = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setUsername(event.target.value)
+  const handleSubmit = () => {
+    const registerData = { email, password, username }
+    axios
+      .post("http://localhost:8080/auth/register", registerData)
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    navigate("/")
+  }
   return (
-    <form>
-      <input type="text" />
-    </form>
+    <Form className="m-5" onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          onChange={onUsername}
+          type="username"
+          required
+          placeholder="Enter username"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          onChange={onEmail}
+          type="email"
+          required
+          placeholder="Enter email"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          onChange={onPassword}
+          type="password"
+          required
+          placeholder="Password"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check
+          type="checkbox"
+          required
+          label="Accept terms and conditions"
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   )
 }
 
